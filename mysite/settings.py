1ad4fb8defbf52988a7721b2deffe9aa096ebfab
cpdepers0n <ssu55555@knou.ac.kr>
2022-01-04 23:41:21 +0900

    회원가입, 글쓴이 표시, 게시물 수정 및 삭제 기능 추가

--- mysite/settings.py
+++ mysite/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'mysite.urls'
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 TEMPLATES = [
     {
--- pybo/views.py
+++ pybo/views.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from django.core.paginator import Paginator
 from .models import Question
 from .forms import QuestionForm, AnswerForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 def index(request):
@@ -28,12 +30,14 @@ def detail(request, question_id):
     return render(request, 'pybo/question_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.method == "POST":
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
@@ -44,11 +48,13 @@ def answer_create(request, question_id):
     return render(request, 'pybo/question_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo:index')
@@ -57,3 +63,26 @@ def question_create(request):
     context = {'form': form}
     return render(request, 'pybo/question_form.html', context)
 
+
+@login_required(login_url="common:login")
+def question_modify(request, question_id):
+    """
+    pybo 질문 수정
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, "수정 권한이 없습니다.")
+        return redirect('pybo:detail', question_id=question.id)
+
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = QuestionForm(instance=question)
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
